b57a6b25ba995cd51fc4dfd7244e5703899196da
Ubuntu <ubuntu@ip-172-31-1-99.ap-south-1.compute.internal>
2024-12-17 17:27:05 +0000

    This is the first version of terraform file

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform { 
+   required_providers {
+       aws = {
+         source = "hashicorp/aws"
+         version = ">= 5.81.0"
+       }
+     }
+     
+     required_version = ">= 2.13.0"    #required_version is nothing but version of cli
+     
+ }    
+     provider "aws" {
+
+        region = "ap-south-1"
+
+     }
+
+
+     resource "aws_instances" "app_server" {
+         ami = "ami-830c94e3"
+         instance_type = "t2.micro"
+         
+         tags = {
+            Name = "Terraform_Demo"
+         }
+     }
+
